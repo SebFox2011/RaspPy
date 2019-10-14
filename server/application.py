c9c399992438a61a97cdd3423acdc035565d69c0
Seb <sebastien.philippe5@gmail.com>
2019-10-14 17:05:44 +0200

    Clignotement de la led sur la pin 18

--- server/application.py
+++ server/application.py
@@ -3,6 +3,18 @@ from flask import render_template  # jinja2
 
 app = Flask(__name__)
 
+import RPi.GPIO as GPIO  # bibliothèque pour utiliser les GPIO
+import time              # bibliothèque pour gestion du temps
+
+GPIO.setmode(GPIO.BCM)   # mode de numérotation des pins
+GPIO.setup(18,GPIO.OUT)  # la pin 18 réglée en sortie (output)
+
+while True:     # boucle répétée jusqu'à l'interruption du programme
+    GPIO.output(18,GPIO.HIGH)   # sortie au niveau logique haut (3.3 V)
+    time.sleep(1)               # on ne change rien pendant 1 seconde
+    GPIO.output(18,GPIO.LOW)    # sortie au niveau logique bas (0 V)
+    time.sleep(1)               # on ne change rien pendant 1 seconde
+
 @app.route('/')
 def index():
     return render_template('index.html')
