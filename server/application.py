73bfe9c9b0b37a61f0fcf5ab9f712299559341c9
Seb <sebastien.philippe5@gmail.com>
2019-10-15 10:52:02 +0200

    Ajout de la commande de 2 led en multi-thread avec le server

--- server/application.py
+++ server/application.py
@@ -1,19 +1,9 @@
 from flask import Flask
 from flask import render_template  # jinja2
+from led import clignote
+from threading import Thread
 
 app = Flask(__name__)
-
-import RPi.GPIO as GPIO  # bibliothèque pour utiliser les GPIO
-import time              # bibliothèque pour gestion du temps
-
-GPIO.setmode(GPIO.BCM)   # mode de numérotation des pins
-GPIO.setup(18,GPIO.OUT)  # la pin 18 réglée en sortie (output)
-
-while True:     # boucle répétée jusqu'à l'interruption du programme
-    GPIO.output(18,GPIO.HIGH)   # sortie au niveau logique haut (3.3 V)
-    time.sleep(1)               # on ne change rien pendant 1 seconde
-    GPIO.output(18,GPIO.LOW)    # sortie au niveau logique bas (0 V)
-    time.sleep(1)               # on ne change rien pendant 1 seconde
 
 @app.route('/')
 def index():
@@ -21,3 +11,7 @@ def index():
 
 if __name__ == '__main__':
     app.run()
+thread = Thread(target = clignote)
+thread.start()
+#thread.join()
+print("thread lancé")
